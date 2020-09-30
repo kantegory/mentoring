76555da94ab9d87c3dfa05e96c1b35c182724c8a
kantegory <davidobryakov@gmail.com>
2020-09-30 19:15:18 +0300

    Change sqlite3 to psql

--- 01_views_examples/example_project/example_project/settings.py
+++ 01_views_examples/example_project/example_project/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'example_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'example_db',
     }
 }
 
