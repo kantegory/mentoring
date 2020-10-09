47a514c1e817092b87c9d3bb0ba3233d9c07dbda
kantegory <davidobryakov@gmail.com>
2020-10-09 10:17:20 +0300

    Add models.Manager example

--- 01_views_examples/example_project/cbv_app/models.py
+++ 01_views_examples/example_project/cbv_app/models.py
@@ -1,13 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class PublisherFilterManager(models.Manager):
+  def get_queryset(self):
+    return super().get_queryset().order_by("-birthdate")
+
+
+class BookCountManager(models.Manager):
+  def get_queryset(self):
+    return super().get_queryset().annotate(published_books_count=models.Count('book'))
+
+
 class Publisher(models.Model):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
   birthdate = models.DateField()
 
+  objects = models.Manager()
+  sorted_objects = PublisherFilterManager()
+  book_count_objects = BookCountManager()
+
   def __str__(self):
     return "{} {}".format(self.first_name, self.last_name)
+
+
 
 
 class Book(models.Model):
