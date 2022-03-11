7f1cbf3339c794a577e9feb428c5c1307ddc32fc
David Dobryakov <davidobryakov@gmail.com>
2022-03-11 14:35:42 +0300

    [feat] Add express + sequelize example

---
+++ 14_express_example/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const db = require('./models')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/users/:id', async (req, res) => {
+  const user = await db.User.findByPk(req.params.id)
+
+  console.log('user is', user)
+
+  if (user) {
+    res.send(user.toJSON())
+  }
+
+  res.send({"msg": "user is not found"})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
