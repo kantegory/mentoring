86d8a523d375a058dad8a0128f128f65906fa3fe
David Dobryakov <davidobryakov@gmail.com>
2024-12-20 16:30:37 +0300

    feat: обновляет пример 20, добавляет гарды на роутер

--- 20_vue3_example/src/router/index.js
+++ 20_vue3_example/src/router/index.js
@@ -12,9 +12,37 @@ const router = createRouter({
       // (до момента открытия этого представления, 
       // оно не будет сохранено в браузере пользователя)
       component: () => import('../views/NotesPage.vue'),
-    }
+      meta: { requiresAuth: false }
+    },
+
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/AuthPage.vue'),
+      meta: { requiresAuth: false }
+    },
+
+    {
+      path: '/private-notes',
+      name: 'privateNotes',
+      // реализация ленивой подгрузки представления
+      // (до момента открытия этого представления, 
+      // оно не будет сохранено в браузере пользователя)
+      component: () => import('../views/NotesPage.vue'),
+      meta: { requiresAuth: true }
+    },
   ]
 })
+
+router.beforeEach((async (to, from) => {
+  if (!to.meta.requiresAuth) {
+    return
+  }
+
+  if (to.meta.requiresAuth && !localStorage.login) {
+    return { name: "login" }
+  }
+}))
 
 // экспортируем сконфигурированный роутер
 export default router
---
+++ 20_vue3_example/src/views/AuthPage.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="d-flex w-100 h-100 justify-content-center align-items-center">
+        <form class="d-flex flex-column my-5">
+            <input
+                type="text"
+                class="my-1"
+                placeholder="username"
+            >
+
+            <input
+                type="password"
+                class="my-1"
+                placeholder="password"
+            >
+
+            <button
+                type="submit"
+                class="btn btn-primary"
+                @click.prevent="setLogin"
+            >
+                Войти
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AuthPage',
+
+  methods: {
+    setLogin() {
+        localStorage.setItem('login', true)
+
+        this.$router.push({ name: "privateNotes" })
+    }
+  },
+
+  mounted() {
+    if (localStorage.login) {
+        this.$router.push({ name: "privateNotes" })
+    }
+  }
+}
+</script>
